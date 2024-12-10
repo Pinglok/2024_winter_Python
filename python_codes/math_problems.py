89688f2995f284b2cfa8cbd3e370519439af0ce7
Pinglok <huang.pinglok@gmail.com>
2024-12-10 21:34:38 +0800

    V1.0

---
+++ python_codes/math_problems.py
@@ -0,0 +1,4 @@
+if __name__=="__main__":
+    print("def")
+else:
+    print("def",__name__)
---
+++ python_codes/sort_algorithm.py
@@ -0,0 +1,31 @@
+import math_problems
+#  bubble sort
+def bubble_sort(numbers, is_log=False): # 預設不印
+    for round in range(len(numbers)-1):
+        for index in range(len(numbers)-1-round):
+            if numbers[index] > numbers[index+1]:
+                numbers[index], numbers[index+1] = \
+                numbers[index+1], numbers[index]
+        if is_log:
+            print(round, numbers)
+    return numbers
+
+# selection sort
+def selection_sort(numbers, is_log=False):
+    for round in range(len(numbers)-1):
+        min_index = round
+        for index in range(round+1, len(numbers)):
+            if numbers[index] < numbers[min_index]:
+                min_index = index
+        if min_index != round:
+            numbers[round], numbers[min_index] = numbers[min_index], numbers[round]
+        if is_log:
+            print(round, numbers)
+    return numbers
+
+if __name__ =="__main__":
+    nums=[40,50,60,30,10]
+    print(bubble_sort(nums))
+    print("ABC")
+else:
+    print("ABC",__name__)
